e2161069d3c613d50a66a7b082f93c5f6e0c2408
Josh Creek <8179928+jcreek@users.noreply.github.com>
2023-07-25 17:32:46 +0100

    feat(*): Add all code due to time constraints

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# PokemonAdventureChatGPTPlugin
-A ChatGPT plugin that allows the user to play an interactive Pokemon adventure game, taking actions in a 'choose your own adventure' style text-based adventure story.
+# Interactive Pokémon Adventure ChatGPT Plugin
+
+The Interactive Pokémon Adventure Plugin is a tool that adds a Pokémon choose your own adventure game into ChatGPT. It uses OpenAI's GPT-3 to generate narratives and responses, and it communicates with the Pokémon API to fetch data about Pokémon for the game.
+
+It was set up for the Semantic Kernel Plugins Hackathon #1.
+
+This plugin has been tested locally but was unable to be tested using the ChatGPT UI due to the ChatGPT UI not being able to load the plugin without a paid subscription and access to the restricted add plugin functionality.
+
+## Features
+
+- Explore: Wander around the Pokémon world, encounter Pokémon and trainers, and make decisions about where to go next.
+- Battle: Engage in Pokémon battles against wild Pokémon and other trainers.
+- Catch: Attempt to catch the wild Pokémon you encounter.
+
+## Usage
+
+Once the plugin is installed, users can interact with it through the ChatGPT UI. They'll start an adventure, then make choices about what action to take next. The plugin will handle their choices and generate narratives and responses with GPT-3.
+
+Here is a very brief example of how a user might interact with the plugin:
+
+1. User: "Start an adventure."
+2. Plugin: (Starts an adventure and fetches a random Pokémon) "You find yourself in a vast meadow. In the distance, you spot a Pikachu. What do you do?"
+3. User: "Battle the Pikachu."
+4. Plugin: (Handles the 'Battle' action and generates a battle narrative) "You send out your Charmander. It uses Ember, dealing a decent amount of damage. Pikachu counters with a quick attack. The battle is on! What's your next move?"
+5. User: "Use a Poké Ball."
+6. Plugin: (Handles the 'Catch' action and generates a catching narrative) "You throw a Poké Ball at the Pikachu. It wiggles once... twice... three times... click! You've successfully caught the Pikachu!"
+
+## Screenshots
+
+### Retrieve Pokemon details from the API
+
+![Screenshot of the plugin](GetPokemonDetails.png "Retrieve Pokemon details from the API")
+
+### Battle a Pokemon, using details from the API
+
+![Screenshot of the plugin](Battle.png "Battle a Pokemon, using details from the API")
+
+### Try to catch a Pokemon, using details from the API
+
+![Screenshot of the plugin](Catch.png "Try to catch a Pokemon, using details from the API")
+
+### Explore the world of Pokemon, using details from the API
+
+![Screenshot of the plugin](Explore.png "Explore the world of Pokemon, using details from the API")
+
+## Disclaimer
+
+This project is a fan-made creation and is not affiliated with the official Pokémon franchise or the Pokémon Company. Pokémon is a trademark of Nintendo, Creatures Inc., and GAME FREAK Inc.
---
+++ azure-function/GetPokemon.cs
@@ -0,0 +1,163 @@
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace skchatgptazurefunction
+{
+    public class GetPokemon
+    {
+        public static readonly HttpClient client = new HttpClient();
+        private readonly ILogger _logger;
+        private readonly IMemoryCache _cache;
+
+        public GetPokemon(ILoggerFactory loggerFactory, IMemoryCache cache)
+        {
+            _logger = loggerFactory.CreateLogger<GetPokemon>();
+            _cache = cache;
+        }
+
+        [OpenApiOperation(operationId: "GetPokemonDetails", tags: new[] { "ExecuteFunction" }, Description = "Gets the details for a given pokemon, or a random one if no name or id is provided")]
+        [OpenApiParameter(name: "pokemonNameOrId", Description = "The name or id of a pokemon, or an empty string", Required = false, In = ParameterLocation.Query)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "The details of either a requested or a random pokemon")]
+        [Function("GetPokemonDetails")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            try
+            {
+                Pokemon pokemon = await GetPokemonDetailsAsync(req.Query["pokemonNameOrId"]);
+
+                HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+                response.Headers.Add("Content-Type", "application/json");
+                response.WriteString(JsonConvert.SerializeObject(pokemon));
+
+                _logger.LogInformation($"Pokemon details were found for {req.Query["pokemonNameOrId"]}");
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+
+                HttpResponseData response = req.CreateResponse(HttpStatusCode.BadRequest);
+                response.Headers.Add("Content-Type", "text/plain");
+                string errorMessage = $"Pokemon details were not found for {req.Query["pokemonNameOrId"] ?? "a random pokemon"}";
+                response.WriteString(errorMessage);
+
+                _logger.LogInformation(errorMessage);
+
+                return response;
+            }
+        }
+
+        public async Task<Pokemon> GetPokemonDetailsAsync(string? pokemonNameOrId)
+        {
+            // Get a random pokemon if none is specified
+            if (string.IsNullOrEmpty(pokemonNameOrId))
+            {
+                int randomId = new Random().Next(1, 898);
+                pokemonNameOrId = randomId.ToString();
+            }
+
+            string url = $"https://pokeapi.co/api/v2/pokemon/{pokemonNameOrId.ToLower()}";
+
+            if (_cache.TryGetValue(url, out Pokemon cachedPokemon))
+            {
+                // Return the cached pokemon if it exists
+                return cachedPokemon;
+            }
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(url);
+                var pokemon = JsonConvert.DeserializeObject<Pokemon>(response);
+
+                // Cache the pokemon permanently
+                _cache.Set(url, pokemon, TimeSpan.FromDays(30));
+
+                return pokemon;
+            }
+        }
+
+    }
+
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int BaseExperience { get; set; }
+        public int Height { get; set; }
+        public int Id { get; set; }
+        public bool IsDefault { get; set; }
+        public string LocationAreaEncounters { get; set; }
+        public int Order { get; set; }
+        public int Weight { get; set; }
+        public List<Ability> Abilities { get; set; }
+        public List<Form> Forms { get; set; }
+        public List<Move> Moves { get; set; }
+        public List<Stat> Stats { get; set; }
+        public List<PokemonType> Types { get; set; }
+    }
+
+    public class Ability
+    {
+        [JsonProperty("ability")]
+        public AbilityDetails AbilityDetails { get; set; }
+        public bool IsHidden { get; set; }
+        public int Slot { get; set; }
+    }
+
+    public class AbilityDetails
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Form
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Move
+    {
+        [JsonProperty("move")]
+        public MoveDetails MoveDetails { get; set; }
+    }
+
+    public class MoveDetails
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Stat
+    {
+        public int BaseStat { get; set; }
+        public int Effort { get; set; }
+        [JsonProperty("stat")]
+        public StatDetails StatDetails { get; set; }
+    }
+
+    public class StatDetails
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class PokemonType
+    {
+        public int Slot { get; set; }
+        public TypeDetails Type { get; set; }
+    }
+
+    public class TypeDetails
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
